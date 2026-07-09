feb7e859f0470ddbe2f97a6b7b432e06fab91d98
bigh20 <166611916+bigh20@users.noreply.github.com>
2026-07-09 22:49:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Site Gladstone-style chargé !");
-});
-
-document.addEventListener("scroll", () => {
-  document.querySelectorAll(".fade-in").forEach(el => {
-    const rect = el.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      el.classList.add("visible");
-    }
-  });
-});
-
 // Animation au scroll
 document.addEventListener("scroll", () => {
   document.querySelectorAll(".fade-in").forEach(el => {
     const rect = el.getBoundingClientRect();
     if (rect.top < window.innerHeight - 100) {
-      el.style.animationPlayState = "running";
+      el.classList.add("visible");
     }
   });
 });
